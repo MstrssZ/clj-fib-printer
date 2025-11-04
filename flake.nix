8ce7d888d49c90f22c427b8856bafae5465faf7c
MstrssZ <nonofyobiznis@gmail.com>
2025-11-04 15:53:04 -0500

    switch to flakes

---
+++ flake.nix
@@ -0,0 +1,23 @@
+
+{
+  description = "A very basic flake";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+
+      devShells.x86_64-linux.default =
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            clojure
+            leiningen
+          ];
+          shellHook = ''
+            export PS1='\[\e[92m\]nix@\h\[\e[0m\]:\[\e[38;5;129m\]\W\[\e[0m\]$(__git_ps1 " (%s)")\\$ '
+                      '';
+        };
+
+    };
+}
