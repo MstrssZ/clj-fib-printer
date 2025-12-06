9666d101e666f21738b639dfe933ae23245c79bf
MstrssZ <nonofyobiznis@gmail.com>
2025-12-06 16:57:52 -0500

    formatting

--- src/fibanocci_printer/main.clj
+++ src/fibanocci_printer/main.clj
@@ -5,24 +5,21 @@
   []
   (println "Useage: executable num\n\t num: max value of fibanocci string to print"))
 
-
-
 (defn fib
   "fib printer"
-  [p1 p2 end] 
-       (cond
-         (> (+ p1 p2) end) (println "done")
-         (= (+ p1 p2) end) (println (+ p1 p2))
-         (< (+ p1 p2) end) (do (println (+ p1 p2)) (fib p2 (+ p2 p1) end))
-          :else (println "Error: issue had")  ))
+  [p1 p2 end]
+  (cond
+    (> (+ p1 p2) end) ()
+    (= (+ p1 p2) end) (println (+ p1 p2))
+    (< (+ p1 p2) end) (do (println (+ p1 p2))
+                          (fib p2 (+ p2 p1) end))
+    :else (println "Error: issue had")))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Take in the highest number to print out."
   [& args]
   (cond
     (<  (count args) 1) (do (println "Error: Did not provide any arguments") (print-usage))
     (>  (count args) 1) (do (println "Error: Provided too many arguments") (print-usage))
     :else
-    ;;(println (first args))
-    (do (fib 0 1 (Integer/parseInt (first args))))
-    ))
+    (fib 0 1 (Integer/parseInt (first args)))))
